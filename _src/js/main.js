f476381bdddecc5657a5beaafb3c46ce60e8be80
rbernier93 <robertbernier93@gmail.com>
2017-06-27 22:22:30 -0400

    Added further functionality to main navigation and began hero module for large screen sizes

--- _src/js/main.js
+++ _src/js/main.js
@@ -1,9 +1,31 @@
 (function ($) {
 
-$('button.navigation-button').click(function() {
-  console.log('triggered');
-  $('.main-navigation').toggleClass('expanded');
-  $('body').toggleClass('fixed');
-});
+  // Navigation Functionality
+  $('button.navigation-button').click(function () {
+    $('.main-navbar').toggleClass('expanded');
+    $('body').toggleClass('fixed');
+  });
+
+  // Navigation collapse on scroll down, then expand on up
+  var initialScroll = $(window).scrollTop();
+
+  $(window).scroll(function () {
+    var interval = setInterval(function () {
+      if ($(window).scrollTop() > initialScroll) {
+        $('.main-navigation').addClass('nav-collapse');
+      }
+      if ($(window).scrollTop() < initialScroll) {
+        $('.main-navigation').removeClass('nav-collapse');
+      }
+      initialScroll = $(window).scrollTop();
+    }, 1000);
+  });
+
+  // Hero Text area Zoom
+  $(document).ready(function () {
+    $('.hero-overlay .overlay-content').addClass('zoomed');
+  });
+
+
 
 })(jQuery);
